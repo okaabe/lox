cb4eb2fa09fc54ffb7e81ea39cd1c3f620b7aeb5
Carlos <54871261+okaabe@users.noreply.github.com>
2021-01-10 19:55:10 -0300

    Removed useless code

--- interpreter/src/main/kotlin/me/otofune/void/interpreter/VoidRuntimeException.kt
+++ interpreter/src/main/kotlin/me/otofune/void/interpreter/VoidRuntimeException.kt
@@ -1,6 +1,5 @@
 package me.otofune.void.interpreter
 
-<<<<<<< HEAD
 import me.otofune.void.front.Token
 import me.otofune.void.front.VoidException
 
@@ -8,6 +7,4 @@ sealed class VoidRuntimeException(message: String) : VoidException("Runtime", me
     class ReferenceError(reference: Any?): VoidRuntimeException("$reference is not defined")
     class HasAlreadyBeenDeclared(name: String): VoidRuntimeException("$name has already been declared")
 }
-=======
-sealed class VoidRuntimeException
->>>>>>> 2bacfc360f8d467d9495aa1b00686de2200f3338
+
