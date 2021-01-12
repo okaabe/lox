976c07658ec6f9ea4f87480fc396bab460f48141
okaabe <orlandi2006@hotmail.com>
2021-01-11 23:22:56 -0300

    Added a variable that has your value leaked from the scope

--- interpreter/src/main/kotlin/me/otofune/void/interpreter/runtime/Environment.kt
+++ interpreter/src/main/kotlin/me/otofune/void/interpreter/runtime/Environment.kt
@@ -5,15 +5,17 @@ import me.otofune.void.interpreter.exceptions.VoidRuntimeException.*
 class Environment() {
     private val variables: MutableMap<String, Any?> = mutableMapOf()
 
-    fun declare(name: String, value: Any?): Any? {
+    fun declare(name: String, value: Any?) {
         if (variables.contains(name)) {
             throw AlreadyDeclared(name)
         }
 
-        return variables[name]
+        variables[name] = value
     }
 
-    fun get(name: String): Any? = variables[name]?.also {
+    fun get(name: String): Any? {
+        if (variables.contains(name)) return variables[name]
+
         throw ReferenceError(name)
     }
 
