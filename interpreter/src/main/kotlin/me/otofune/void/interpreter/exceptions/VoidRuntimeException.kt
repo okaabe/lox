5a56484bb33118423d167d8dff9efccdbcc9eb92
okaabe <orlandi2006@hotmail.com>
2021-01-14 23:39:03 -0300

    Added two new exceptions

--- interpreter/src/main/kotlin/me/otofune/void/interpreter/exceptions/VoidRuntimeException.kt
+++ interpreter/src/main/kotlin/me/otofune/void/interpreter/exceptions/VoidRuntimeException.kt
@@ -1,9 +1,12 @@
 package me.otofune.void.interpreter.exceptions
 
+import me.otofune.void.grammar.FrontException
 import me.otofune.void.grammar.VoidException
 
 sealed class VoidRuntimeException(message: String) : VoidException("Runtime", message) {
     class AlreadyDeclared(reference: Any?): VoidRuntimeException("$reference is already been declared")
     class ReferenceError(reference: Any?) : VoidRuntimeException("$reference is not defined")
     class InvalidOperand(operator: String, operand: Any?): VoidRuntimeException("$operator $operand - The operand must be a number")
+    class InvalidCalle(calle: Any?) : VoidRuntimeException("$calle, you can only call functions and classes")
+    class InvalidArgumentsAmount(expected: Int, got: Int): VoidRuntimeException("Expected $expected arguments, but got $got")
 }
