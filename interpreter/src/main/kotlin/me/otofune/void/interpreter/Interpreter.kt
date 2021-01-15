63e6e3488f3f4691d37ecc35b9d3c9f09d98f175
okaabe <orlandi2006@hotmail.com>
2021-01-15 13:14:55 -0300

    Added the builtin setup in environment

--- interpreter/src/main/kotlin/me/otofune/void/interpreter/Interpreter.kt
+++ interpreter/src/main/kotlin/me/otofune/void/interpreter/Interpreter.kt
@@ -1,11 +1,14 @@
 package me.otofune.void.interpreter
 
 import me.otofune.void.grammar.Stmt
+import me.otofune.void.interpreter.runtime.Builtin
 import me.otofune.void.interpreter.runtime.Environment
 import me.otofune.void.interpreter.runtime.Evaluator
 
 class Interpreter {
-    private val environment = Environment()
+    private val environment = Environment().also {
+        Builtin(it).setup()
+    }
     private val evaluator = Evaluator(environment)
 
     fun interpret(statements: List<Stmt>) {
