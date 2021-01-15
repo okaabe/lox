92288e39226cc50eb56e1b879ff565cf25203037
okaabe <orlandi2006@hotmail.com>
2021-01-14 23:37:21 -0300

    Added new front exception

--- grammar/src/main/kotlin/me/otofune/void/grammar/FrontException.kt
+++ grammar/src/main/kotlin/me/otofune/void/grammar/FrontException.kt
@@ -6,4 +6,5 @@ sealed class FrontException(message: String) : VoidException("Front", message) {
     class ExpectedTokenButGot(expected: TokenType, got: TokenType, line: Int) : FrontException("Expected token $expected but got $got in line $line")
     class InvalidExpression(line: Int) : FrontException("Unexpected/Invalid expression in line $line")
     class InvalidAssignmentTarget(line: Int) : FrontException("Invalid assignment target in line $line")
+    class ParamsLimitExceeded(line: Int): FrontException("Params limit exceeded in line $line, functions can't have more than $MAX_PARAM parameters")
 }
