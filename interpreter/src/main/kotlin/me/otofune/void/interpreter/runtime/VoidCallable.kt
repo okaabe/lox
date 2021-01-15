7ddab5f69f013b8599f2e18f4bb1976f0f82c4fe
okaabe <orlandi2006@hotmail.com>
2021-01-15 13:13:45 -0300

    Added a new callable(native functions)

--- interpreter/src/main/kotlin/me/otofune/void/interpreter/runtime/VoidCallable.kt
+++ interpreter/src/main/kotlin/me/otofune/void/interpreter/runtime/VoidCallable.kt
@@ -6,7 +6,7 @@ sealed class VoidCallable {
     abstract fun call(evaluator: Evaluator, arguments: List<Any?>): Any?
     abstract fun arity(): Int
 
-    class VoidFunction(
+    data class VoidFunction(
         private val statement: Stmt.FunctionStmt,
         private val environment: Environment
     ): VoidCallable() {
@@ -25,5 +25,19 @@ sealed class VoidCallable {
         }
 
         override fun toString(): String = "Function ${statement.name.lexeme}"
+    }
+
+    data class VoidNativeFunction(
+        val name: String,
+        private val paramsSize: Int,
+        private val callback: (args: List<Any?>) -> Any?
+    ): VoidCallable() {
+        override fun arity(): Int = paramsSize
+
+        override fun call(evaluator: Evaluator, arguments: List<Any?>): Any? {
+            return callback(arguments)
+        }
+
+        override fun toString(): String = "Native function $name"
     }
 }
