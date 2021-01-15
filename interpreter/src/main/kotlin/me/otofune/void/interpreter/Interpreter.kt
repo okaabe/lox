fc4bdacbb191a129fd058327c9fb31b8d279a5eb
okaabe <orlandi2006@hotmail.com>
2021-01-14 23:37:54 -0300

    Substituted executor to evaluator

--- interpreter/src/main/kotlin/me/otofune/void/interpreter/Interpreter.kt
+++ interpreter/src/main/kotlin/me/otofune/void/interpreter/Interpreter.kt
@@ -3,12 +3,10 @@ package me.otofune.void.interpreter
 import me.otofune.void.grammar.Stmt
 import me.otofune.void.interpreter.runtime.Environment
 import me.otofune.void.interpreter.runtime.Evaluator
-import me.otofune.void.interpreter.runtime.Executor
 
 class Interpreter {
     private val environment = Environment()
     private val evaluator = Evaluator(environment)
-    private val executor = Executor(evaluator, environment)
 
     fun interpret(statements: List<Stmt>) {
         statements.map { statement ->
@@ -17,6 +15,6 @@ class Interpreter {
     }
 
     private fun execute(statement: Stmt) {
-        executor.visitStmt(statement)
+        evaluator.visitStmt(statement)
     }
 }
