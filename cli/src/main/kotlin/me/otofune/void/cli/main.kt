1fa4894c39bdeb14fb933280000b71f0acb76a5e
okaabe <orlandi2006@hotmail.com>
2021-01-20 00:09:31 -0300

    Removed old main file

--- cli/src/main/kotlin/me/otofune/void/cli/main.kt
+++
@@ -1,9 +0,0 @@
-package me.otofune.void.cli
-
-fun main(args: Array<String>) {
-    if (args.size > 1 || args.isEmpty()) {
-        return println("Usage: void [script]")
-    }
-
-    CodeExecutor.findAndExecuteFile(args[0])
-}
