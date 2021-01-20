91c5437c46a4d00658b579f6bc5630ce43ceed4c
okaabe <orlandi2006@hotmail.com>
2021-01-20 00:08:53 -0300

    Added the resolver

--- interpreter/src/main/kotlin/me/otofune/void/interpreter/resolver/Resolver.kt
+++ interpreter/src/main/kotlin/me/otofune/void/interpreter/resolver/Resolver.kt
@@ -2,68 +2,139 @@ package me.otofune.void.interpreter.resolver
 
 import me.otofune.void.grammar.Expr
 import me.otofune.void.grammar.Stmt
+import me.otofune.void.grammar.Token
 import me.otofune.void.interpreter.Interpreter
+import me.otofune.void.interpreter.exceptions.LoxResolverException
+import java.util.*
 
 class Resolver(
     private val interpreter: Interpreter,
 ): Stmt.Visitor<Any?>, Expr.Visitor<Any?> {
+    private val scopes: Stack<MutableMap<String, Boolean>> = Stack()
+    private var location: Location = Location.NONE
+
     fun resolve(statements: List<Stmt>) = statements.map {
         visitStmt(it)
     }
 
+
     override fun visitBlockStmt(stmt: Stmt.BlockStmt) {
-        TODO("Not yet implemented")
+        beginScope()
+        resolve(stmt.statements)
+        endScope()
     }
 
-    override fun visitExprStmt(stmt: Stmt.ExprStmt) {
-        TODO("Not yet implemented")
-    }
+    override fun visitExprStmt(stmt: Stmt.ExprStmt) = visitExpr(stmt.expr)
 
     override fun visitVarStmt(stmt: Stmt.VarStmt) {
-        TODO("Not yet implemented")
+        declare(stmt.name)
+        visitExpr(stmt.value)
+        define(stmt.name)
     }
 
     override fun visitIfStmt(stmt: Stmt.IfStmt) {
-        TODO("Not yet implemented")
+        visitExpr(stmt.condition)
+        visitStmt(stmt.thenDo)
+        visitStmt(stmt.elseDo!!)
     }
 
     override fun visitFunctionStmt(stmt: Stmt.FunctionStmt) {
-        TODO("Not yet implemented")
+        declare(stmt.name)
+        define(stmt.name)
+
+        resolveFunction(stmt, Location.FUNCTION)
     }
 
     override fun visitReturnStmt(stmt: Stmt.ReturnStmt) {
-        TODO("Not yet implemented")
+        if (location == Location.NONE) {
+            throw LoxResolverException.IllegalReturnStatement()
+        }
+
+        visitExpr(stmt.expression!!)
     }
 
     override fun visitWhileStmt(stmt: Stmt.WhileStmt) {
-        TODO("Not yet implemented")
+        visitExpr(stmt.condition)
+        visitStmt(stmt.body)
     }
 
     override fun visitAssignExpr(expr: Expr.Assign) {
-        TODO("Not yet implemented")
+        visitExpr(expr.value)
+        resolveLocal(expr, expr.target.variable)
     }
 
     override fun visitBinaryExpr(expr: Expr.Binary) {
-        TODO("Not yet implemented")
+        visitExpr(expr.left)
+        visitExpr(expr.right)
     }
 
     override fun visitCallExpr(expr: Expr.Call) {
-        TODO("Not yet implemented")
+        visitExpr(expr.calle)
+
+        expr.arguments.map { arg ->
+            visitExpr(arg)
+        }
     }
 
-    override fun visitGroupingExpr(expr: Expr.Grouping) {
-        TODO("Not yet implemented")
-    }
+    override fun visitGroupingExpr(expr: Expr.Grouping) = visitExpr(expr.expr)
 
-    override fun visitLiteralExpr(expr: Expr.Literal) {
-        TODO("Not yet implemented")
-    }
+    override fun visitLiteralExpr(expr: Expr.Literal) {}
 
-    override fun visitUnaryExpr(expr: Expr.Unary) {
-        TODO("Not yet implemented")
-    }
+    override fun visitUnaryExpr(expr: Expr.Unary) = visitExpr(expr.right)
 
     override fun visitVariableExpr(expr: Expr.Variable) {
-        TODO("Not yet implemented")
+        if (!scopes.isEmpty() && scopes.peek()[expr.variable.lexeme] == false) {
+            throw LoxResolverException.IllegalVariableInitializer(expr.variable.lexeme, expr.variable.line)
+        }
+
+        resolveLocal(expr, expr.variable)
     }
+
+    private fun resolveLocal(expr: Expr, name: Token) {
+        for(i in scopes.size - 1 downTo 0) {
+            if (scopes[i].containsKey(name.lexeme)) {
+                interpreter.resolve(expr, scopes.size - 1 - i)
+            }
+        }
+    }
+
+    private fun resolveFunction(stmt: Stmt.FunctionStmt, loc: Location) {
+        val enclosingLocation = this.location
+        location = loc
+
+        beginScope()
+
+        stmt.params.map {
+            declare(it)
+            define(it)
+        }
+
+        resolve(stmt.body)
+
+        endScope()
+        location = enclosingLocation
+    }
+
+    private fun declare(name: Token) {
+        if (scopes.isEmpty()) {
+            return
+        }
+
+        scopes.peek().also {
+            it[name.lexeme] = false
+        }
+    }
+
+    private fun define(name: Token) {
+        if (scopes.isEmpty()) {
+            return
+        }
+
+        scopes.peek().also {
+            it[name.lexeme] = true
+        }
+    }
+
+    private fun beginScope() = scopes.push(mutableMapOf())
+    private fun endScope() = scopes.pop()
 }
