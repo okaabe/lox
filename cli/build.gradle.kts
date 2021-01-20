8069e616fdf9854baf37aeb439e524583c6134d2
okaabe <orlandi2006@hotmail.com>
2021-01-20 00:13:02 -0300

    Added new dependency and changed the mainClass

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -9,17 +9,18 @@ group = "me.otofune.void.cli"
 version = "1.0-SNAPSHOT"
 
 application {
-    mainClass.set("me.otofune.void.cli.MainKt")
+    mainClass.set("me.otofune.void.cli.ApplicationKt")
 }
 
 dependencies {
+    implementation("com.github.ajalt.clikt:clikt:3.1.0")
     api(project(":grammar"))
     api(project(":interpreter"))
 }
 
 val jar by tasks.getting(Jar::class) {
     manifest {
-        attributes["Main-Class"] = "me.otofune.void.cli.MainKt"
+        attributes["Main-Class"] = "me.otofune.void.cli.ApplicationKt"
     }
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }) {
         exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
